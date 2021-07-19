67c9a44e4d7d785a7a54f8678896e2e44052081d
radenibnu <radenibnuqc@gmail.com>
2021-07-19 13:30:14 +0700

    replace destination file for frontend controller

--- app/Http/Controllers/frontendController.php
+++ app/Http/Controllers/frontendController.php
@@ -13,7 +13,7 @@ class frontendController extends Controller
      */
     public function index()
     {
-        return view('pages.home');
+        return view('pages.frontend.home');
     }
 
     /**

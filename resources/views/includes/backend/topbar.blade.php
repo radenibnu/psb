ca8e2411d7c9af46e4510fb919d7fd07a1525c3d
radenibnu <radenibnuqc@gmail.com>
2021-08-01 14:31:05 +0700

    delete image on topbar backend

--- resources/views/includes/backend/topbar.blade.php
+++ resources/views/includes/backend/topbar.blade.php
@@ -64,7 +64,7 @@
             <li class="nav-item dropdown">
                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                     <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->name }}</span>
-                    <img class="img-profile rounded-circle">
+                    {{-- <img class="img-profile rounded-circle"> --}}
                         {{-- src="{{ Auth::$user->image }}"> --}}
                 </a>
 
